5b2af367c98f26d01ef98517b272695996b1b19b
Frank Leonardo Puma Chara <frank.puma@innovaritp.com>
2021-05-25 23:39:19 -0500

    added start:dev script to package.json

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { app, routes } from "./app";
 import { CommonRoutes } from "./common/routes/common.routes";
 
-app.listen(3000, () => {
-    console.log(`Server running at http://localhost:${3000}`)
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
     routes.forEach((route: CommonRoutes) => {
         console.log(`Routes configured for  ${route.getName()}`)
     })
