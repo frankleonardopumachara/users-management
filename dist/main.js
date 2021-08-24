d539d2b888c7ca3a7861f5587207d92653f05a09
Frank Leonardo Puma Chara <frank.puma@innovaritp.com>
2021-08-23 23:00:07 -0500

    deleted dist directory

--- dist/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-app_1.app.listen(3000, () => {
-    console.log(`Server running at http://localhost:${3000}`);
-    app_1.routes.forEach((route) => {
-        console.log(`Routes configured for  ${route.getName()}`);
-    });
-});
